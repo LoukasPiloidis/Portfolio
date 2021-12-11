43a93bba1943b0feeebe0f90fe8c1e0650cf45fb
salt-dev <loukasmalias@hotmail.com>
2021-12-11 11:05:34 +0100

    browser router initialized

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <nav class="navbar__list">
-      <a class="navbar__item" href="#">Home</a>
-      <a class="navbar__item" href="#">About</a>
-      <a class="navbar__item" href="#">Projects</a>
-      <a class="navbar__item" href="#">Contactd</a>
-    </nav>
+    <>
+      <nav class="navbar__list">
+        <Link to="/" class="navbar__item">Home</Link>
+        <Link to="/about" class="navbar__item">About</Link>
+        <Link to="/projects" class="navbar__item">Projects</Link>
+        <Link to="/contact" class="navbar__item">Contact</Link>
+      </nav>
+      <Routes>
+        {/* <Route path="/" element={<App />} /> */}
+      </Routes>
+    </>
   );
 }
 
