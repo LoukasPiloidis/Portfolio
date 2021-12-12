51eed387c013a7af4661c46a7ba16cc1781b79a6
salt-dev <loukasmalias@hotmail.com>
2021-12-12 11:10:26 +0100

    cleanup of unnecessary packages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <>
-      <nav class="navbar__list">
-        <Link to="/" class="navbar__item">Home</Link>
-        <Link to="/about" class="navbar__item">About</Link>
-        <Link to="/projects" class="navbar__item">Projects</Link>
-        <Link to="/contact" class="navbar__item">Contact</Link>
-      </nav>
-      <Routes>
-        {/* <Route path="/" element={<App />} /> */}
-      </Routes>
-    </>
+    <nav class="navbar__list">
+      <a to="#" class="navbar__item">Home</a>
+      <a to="#" class="navbar__item">About</a>
+      <a to="#" class="navbar__item">Projects</a>
+      <a to="#" class="navbar__item">Contact</a>
+    </nav>
   );
 }
 
